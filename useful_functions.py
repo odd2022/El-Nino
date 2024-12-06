2ee1fcd4f98d7d6232479b75eef0176cf1bd4af6
odd2022 <audrey.airaud@gmail.com>
2024-12-06 10:25:15 +0100

    added nathan's work

---
+++ useful_functions.py
@@ -0,0 +1,51 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+
+def get_data(Path):
+    """
+    Path: Path of the preprocessed data you want to get
+    """
+    data = np.load(Path)
+    # Get the variables
+    #print(data.files)
+    X = data["X"]
+    y = data["y"]
+
+    # Check dimensions
+    print('==== GET THE DATA ====')
+    print("Shape of X:", X.shape)
+    print("Shape of y:", y.shape)
+
+    print("First 25 elements of y:", y[:25])
+    return(X,y)
+
+def split_scale_data(X, y, RANDOM_SPLIT=False, train_size=374, verbose=True):
+    """
+    X, y: Inputs and outputs
+    RANDOM_SPLIT: Boolean, if False splits for train and test are done in in time order, if True, splits are random
+    train_size: number of data in the train sample (test_size=474 - train_size)
+    verbose: if True, the function prints information about the shape of the outputs data sets
+    """
+    # Split the data
+    if RANDOM_SPLIT:
+        X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_size, random_state=42)
+    else:
+        X_train, X_test, y_train, y_test = X[:train_size], X[train_size:], y[:train_size], y[train_size:]
+
+    # Introduce a scaler
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    # Standardize the train and test data
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    # Check dimensions
+    if verbose:
+        print("==== SPLIT & SCALE THE DATA ====")
+        print("Shape of X_train:", X_train.shape)
+        print("Shape of y_train:", y_train.shape)
+        print("Shape of X_test:", X_test.shape)
+        print("Shape of y_test:", y_test.shape)
+
+    return(X_train, X_test, y_train, y_test)
